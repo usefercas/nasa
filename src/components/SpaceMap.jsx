74b326cc2b0e81ae186ffd73092d9b537c07cce4
usefercas <124436934+usefercas@users.noreply.github.com>
2024-06-21 19:33:57 +0200

    siguiente

--- src/components/SpaceMap.jsx
+++ src/components/SpaceMap.jsx
@@ -4,42 +4,50 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const SpaceMap = () => {
-  const [events, setEvents] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchEvents = async () => {
+    const fetchCities = async () => {
+      const apiKey = 'da71f974e89e6a9b12c190077ff6c259';
+      const apiUrl = `https://api.openweathermap.org/data/2.5/box/city?bbox=-180,-90,180,90,10&appid=${apiKey}`;
+
       try {
-        const response = await axios.get('https://eonet.sci.gsfc.nasa.gov/api/v3/events');
-        setEvents(response.data.events);
+        const response = await axios.get(apiUrl);
+        setCities(response.data.list);
       } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error('Error fetching cities:', error);
+        setError('Failed to fetch cities. Please try again later.');
       }
     };
 
-    fetchEvents();
+    fetchCities();
   }, []);
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {events.map((event) => (
-          <Marker key={event.id} position={[event.geometries[0].coordinates[1], event.geometries[0].coordinates[0]]}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {cities.map(city => (
+          <Marker key={city.id} position={[city.coord.lat, city.coord.lon]}>
             <Popup>
               <div style={{ maxWidth: '300px' }}>
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <p>Category: {event.categories[0].title}</p>
-                <p>Date: {new Date(event.geometries[0].date).toLocaleDateString()}</p>
+                <h3>{city.name}</h3>
+                <p>Temperature: {city.main.temp} &#8451;</p>
+                <p>Weather: {city.weather[0].description}</p>
               </div>
             </Popup>
           </Marker>
         ))}
       </MapContainer>
+      {error && (
+        <div style={{ position: 'absolute', top: '10px', left: '10px', backgroundColor: 'rgba(255, 0, 0, 0.5)', padding: '10px', borderRadius: '5px', zIndex: 1000 }}>
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 };
 
 export default SpaceMap;
+
